11c15614944522bed59eaadb5f59b0489cc118c5
ImSreyas <sreyassatheesh755@gmail.com>
2024-06-05 22:10:32 +0530

    Structural change.

--- zeo.sh
+++ zeo.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
-# shell targets 
 
+final_target=~/userfiles
+
+# Shell targets 
 bash_target=~/.bashrc
 fish_target=~/.config/fish/config.fish
 zsh_target=~/.zshrc
 
-final_target=~/userfiles
-
-# creating user files directory if not already exists
+# Creating user files directory if not already exists
 if ! [[ -d $final_target ]]; then
     if [[ -f $final_target ]]; then 
         rm $final_target
@@ -15,24 +15,28 @@ if ! [[ -d $final_target ]]; then
     mkdir -p $final_target
 fi
 
-if test -f $bash_target; then
-    while true; do 
-        read -p "Do you want to backup bash config file to dotfiles? (y/n) : " confirmation 
-        case $confirmation in 
-            y)
-                cp $bash_target $final_target 
-                echo "Bash config file copied to $final_target"
-                break 
-                ;;
-            n)
-                echo "No changes are made..."
-                break
-                ;;
-            *)
-                echo "Invalid input..."
-                ;;
-        esac
-    done
-else 
-    echo "Bash file not found..."
-fi
+bash_sync() {
+    if test -f $bash_target; then
+        while true; do 
+            read -p "Do you want to backup bash config file to dotfiles? (y/n) : " confirmation 
+            case $confirmation in 
+                y)
+                    cp $bash_target $final_target 
+                    echo "Bash config file copied to $final_target"
+                    break 
+                    ;;
+                n)
+                    echo "No changes are made..."
+                    break
+                    ;;
+                *)
+                    echo "Invalid input..."
+                    ;;
+            esac
+        done
+    else 
+        echo "Bash file not found..."
+    fi
+}
+
+bash_sync 
