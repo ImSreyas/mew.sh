da14def43228efc15d895fcf8030c410cb3eabdc
ImSreyas <sreyassatheesh755@gmail.com>
2024-06-06 22:34:39 +0530

    Main file improved.

--- mew.sh
+++ mew.sh
@@ -28,17 +28,18 @@ function print_symbol_line() {
     done; echo
 }
 
-# Errors 
+# !Errors 
+# Source file not found error message 
+function source_err() {
+    echo "File not found : \e[31m$1\e[0m at $2"
+}
 # Common Err fix
 function common_err_fix() {
     echo -e "\nCommon Error fix\n----------------"
     echo -e "Reinstall : \e[33m git clone https://github.com/ImSreyas/mew.git && cd mew && ./install.sh\e[0m\n"
 }
-# Source file not found error message 
-function source_err() {
-    echo "File not found : \e[31m$1\e[0m at $2"
-}
 
+# ? Program starts here 
 print_symbol_line "#"
 
 # Importing SOURCE files 
@@ -53,16 +54,16 @@ if [[ -f $bash_sync_target ]]; then
     source $bash_sync_target
     bash_sync 
 else 
-    err_str+="$(source_err "bash_sync.sh" "$lib_target/shell/bash_sync.sh")\n"
+    err_str+="$(source_err "bash_sync.sh" "$lib_target/shell/bash_sync.sh")\n" # Appending error 
 fi
 # Fish 
 if [[ -f $fish_sync_target ]]; then
     source $fish_sync_target
     fish_sync 
 else 
-    err_str+="$(source_err "fish_sync.sh" "$lib_target/shell/fish_sync.sh")"
+    err_str+="$(source_err "fish_sync.sh" "$lib_target/shell/fish_sync.sh")" # Appending error 
 fi
-# Checking if any errors found 
+# CHECKING and DISPLAYING any Errors if found 
 if [[ "$err_str" != "" ]]; then 
     echo -e "\nErrors\n------"
     echo -e $err_str
@@ -70,3 +71,4 @@ if [[ "$err_str" != "" ]]; then
 fi
 
 print_symbol_line "#"
+# ? Program ends here
