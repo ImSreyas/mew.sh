95e850a2ecc4ca74bbf3d189135927ea67c4a329
ImSreyas <sreyassatheesh755@gmail.com>
2024-06-08 18:11:20 +0530

    refactor: Made Modular.

--- bin/mew.sh
+++ bin/mew.sh
@@ -56,7 +56,7 @@ function print_symbol_line() {
 # !Errors 
 # Source file not found error message 
 function source_err() {
-    echo "File not found : $(get_color_code "red")$1$(get_color_code "unset") at $2"
+    echo "File not found : $(get_color_code "red")${1:-"Unknown"}$(get_color_code "unset") at ${2:-"Path unknown"}"
 }
 # Common Err fix
 function common_err_fix() {
@@ -70,6 +70,13 @@ function common_err_fix() {
     echo -e "$(get_color_code "unset")" # Unsetting yellow color
     echo
 }
+function error_provider() {
+    err_string=${1:-"Unknown Error"}
+    echo -e "\nErrors"
+    print_symbol_line "-" 6
+    echo -e $err_string
+    common_err_fix
+}
 
 function header() {
     print_symbol_line "#" 8 "cyan"
@@ -81,8 +88,6 @@ function footer() {
 # ? Program starts here 
 header
 
-echo "Select config files for creating backup!"
-
 # Importing SOURCE files 
 # Targets 
 bash_sync_target=$lib_target/shell/bash_sync.sh
@@ -90,35 +95,54 @@ fish_sync_target=$lib_target/shell/fish_sync.sh
 zsh_sync_target=$lib_target/shell/zsh_sync.sh
 
 # Checking availability of the SOURCE files 
-# Bash 
 err_str=""
+
+# Question 
+question_path="../lib/util/question.sh"
+if [[ -f $question_path ]]; then 
+    source $question_path
+else err_str+="$(source_err "question.sh" "mew/lib/util/question.sh")\n" # Appending error 
+fi
+
+# Updater 
+updater_path="../lib/util/updater.sh"
+if [[ -f $updater_path ]]; then 
+    source $updater_path
+else err_str+="$(source_err "updater.sh" "mew/lib/util/updater.sh")" # Appending error 
+fi
+
+# Checking Question or Updater source files are missing
+if [[ "$err_str" != "" ]]; then 
+    error_provider "$err_str"
+    footer
+    exit 0
+fi
+
+echo "Select config files for creating backup!"
+
+# Bash 
 if [[ -f $bash_sync_target ]]; then
     source $bash_sync_target
     bash_sync 
-else 
-    err_str+="$(source_err "bash_sync.sh" "mew/lib/shell/bash_sync.sh")\n" # Appending error 
+else err_str+="$(source_err "bash_sync.sh" "mew/lib/shell/bash_sync.sh")\n" # Appending error 
 fi
+
 # Fish 
 if [[ -f $fish_sync_target ]]; then
     source $fish_sync_target
     fish_sync 
-else 
-    err_str+="$(source_err "fish_sync.sh" "mew/lib/shell/fish_sync.sh")\n" # Appending error 
+else err_str+="$(source_err "fish_sync.sh" "mew/lib/shell/fish_sync.sh")\n" # Appending error 
 fi
 # Zsh
 if [[ -f $zsh_sync_target ]]; then
     source $zsh_sync_target
     zsh_sync 
-else 
-    err_str+="$(source_err "zsh_sync.sh" "mew/lib/shell/zsh_sync.sh")" # Appending error 
+else err_str+="$(source_err "zsh_sync.sh" "mew/lib/shell/zsh_sync.sh")" # Appending error 
 fi
 
 # CHECKING and DISPLAYING any Errors if found 
 if [[ "$err_str" != "" ]]; then 
-    echo -e "\nErrors"
-    print_symbol_line "-" 6
-    echo -e $err_str
-    common_err_fix
+    error_provider "$err_str"
 fi
 
 footer
