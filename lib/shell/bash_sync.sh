bae4b93bc033b0c74309a6188a9da82bfeed0e13
ImSreyas <sreyassatheesh755@gmail.com>
2024-06-09 19:12:23 +0530

    chore: Output target of bash files changed to shell folder

--- lib/shell/bash_sync.sh
+++ lib/shell/bash_sync.sh
@@ -1,4 +1,4 @@
 function bash_sync() {
     # Main call
-    sync_file .bashrc ~/.bashrc "Bash file?" # $1) filename $2) file path $3) question 
+    sync_file .bashrc ~/.bashrc "Bash file?" $final_target/shell # $1) filename $2) file path $3) question $4) output target 
 }
