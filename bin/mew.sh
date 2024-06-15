d8bafccb53053569df821a590a610f4f38aef3ca
ImSreyas <sreyassatheesh755@gmail.com>
2024-06-15 20:11:52 +0530

    chore: mew push feature added

--- bin/mew.sh
+++ bin/mew.sh
@@ -79,128 +79,146 @@ function error_provider() {
 }
 
 function header() {
-    print_symbol_line "#" 8 "cyan"
-    echo
+    print_symbol_line "#" 8 "magenta"
+    # echo
 }
 function footer() {
     echo
-    print_symbol_line "#" 8 "cyan"
+    print_symbol_line "#" 8 "magenta"
+}
+
+function fetch_files() {
+    
+    err_str=""
+
+    # Question 
+    question_source_path=$lib_target/util/question.sh # Source targets 
+    if [[ -f $question_source_path ]]; then 
+        source $question_source_path
+    else err_str+="$(source_err "question.sh" "mew/lib/util/question.sh")\n" # Appending error 
+    fi
+
+    # Updater 
+    updater_source_source_path=$lib_target/util/updater.sh # Source targets  # Source targets 
+    if [[ -f $updater_source_source_path ]]; then 
+        source $updater_source_source_path
+    else err_str+="$(source_err "updater.sh" "mew/lib/util/updater.sh")" # Appending error 
+    fi
+
+    # Checking Question or Updater source files are missing
+    if [[ "$err_str" != "" ]]; then 
+        error_provider "$err_str"
+        # footer
+        # exit 0 # There is no point in continuing if either Question or Updater source file is missing
+    else 
+        # echo "Select files for creating backup!"
+
+        # Bash 
+        bash_target=~/.bashrc # Actual file target
+        bash_source_path=$lib_target/shell/bash_sync.sh # Source targets 
+
+        if [[ -f $bash_target ]]; then # No need to ask for a backup, if the user don't have .bashrc file
+            if [[ -f $bash_source_path ]]; then
+                source $bash_source_path
+                bash_sync $1
+                # if [[ $val -eq 1 ]]; then
+                #     echo "Working"
+                # else 
+                #     echo "Not working"
+                # fi
+            else err_str+="$(source_err "bash_sync.sh" "mew/lib/shell/bash_sync.sh")\n" # Appending error 
+            fi
+        fi
+
+        # Fish
+        fish_target=~/.config/fish/config.fish # Actual file target
+        fish_source_path=$lib_target/shell/fish_sync.sh # Source targets 
+
+        if [[ -f $fish_target ]]; then  # No need to ask for a backup, if the user don't have config.fish file
+            if [[ -f $fish_source_path ]]; then
+                source $fish_source_path
+                fish_sync $1
+            else err_str+="$(source_err "fish_sync.sh" "mew/lib/shell/fish_sync.sh")\n" # Appending error 
+            fi
+        fi
+
+        # Zsh
+        zsh_target=~/.zshrc # Actual file target
+        zsh_source_path=$lib_target/shell/zsh_sync.sh # Source targets 
+
+        if [[ -f $zsh_target ]]; then  # No need to ask for a backup, if the user don't have .zshrc file
+            if [[ -f $zsh_source_path ]]; then
+                source $zsh_source_path
+                zsh_sync $1
+            else err_str+="$(source_err "zsh_sync.sh" "mew/lib/shell/zsh_sync.sh")" # Appending error 
+            fi
+        fi
+
+        # Vim 
+        vim_target=~/.vimrc # Actual file target
+        vim_source_path=$lib_target/vim/vim_sync.sh # Source targets 
+
+        if [[ -f $vim_target ]]; then  # No need to ask for a backup, if the user don't have .vimrc file
+            if [[ -f $vim_source_path ]]; then
+                source $vim_source_path
+                vim_sync $1
+            else err_str+="$(source_err "vim_sync.sh" "mew/lib/vim/vim_sync.sh")" # Appending error 
+            fi
+        fi
+
+
+        # Vscode user settings
+        vscode_user_settings_target=~/.config/Code/User/settings.json # Actual file target
+        vscode_user_settings_source_path=$lib_target/vscode/user_settings_sync.sh # Source targets 
+
+        if [[ -f $vscode_user_settings_target ]]; then  # No need to ask for a backup, if the user don't have settings.json file
+            if [[ -f $vscode_user_settings_source_path ]]; then
+                source $vscode_user_settings_source_path
+                vscode_user_settings_sync $1
+            else err_str+="$(source_err "user_settings_sync.sh" "mew/lib/vscode/user_settings_sync.sh")" # Appending error 
+            fi
+        fi
+
+        # dnf 
+        dnf_source_path=$lib_target/package-manager/dnf_sync.sh # Source targets 
+
+        if command -v dnf > /dev/null 2>&1; then  # No need to ask for a backup, if the user don't have dnf package manager (a non RHEL/Fedora user)
+            if [[ -f $dnf_source_path ]]; then
+                source $dnf_source_path
+                dnf_sync $1
+            else err_str+="$(source_err "dnf_sync.sh" "mew/lib/dnf/dnf_sync.sh")" # Appending error 
+            fi
+        fi
+
+        # Lf 
+        lf_target=~/.config/lf/lfrc # Actual file target
+        lf_source_path=$lib_target/lf/lf_sync.sh # Source targets 
+
+        if [[ -f $lf_target ]]; then  # No need to ask for a backup, if the user don't have lfrc file
+            if [[ -f $lf_source_path ]]; then
+                source $lf_source_path
+                lf_sync $1
+            else err_str+="$(source_err "lf_sync.sh" "mew/lib/lf/lf_sync.sh")" # Appending error 
+            fi
+        fi
+
+        # CHECKING and DISPLAYING any Errors if found 
+        if [[ "$err_str" != "" ]]; then 
+            error_provider "$err_str"
+        fi
+    fi
 }
 
 # ? Program starts here 
 header
 
-err_str=""
-
-# Question 
-question_source_path=$lib_target/util/question.sh # Source targets 
-if [[ -f $question_source_path ]]; then 
-    source $question_source_path
-else err_str+="$(source_err "question.sh" "mew/lib/util/question.sh")\n" # Appending error 
-fi
-
-# Updater 
-updater_source_source_path=$lib_target/util/updater.sh # Source targets  # Source targets 
-if [[ -f $updater_source_source_path ]]; then 
-    source $updater_source_source_path
-else err_str+="$(source_err "updater.sh" "mew/lib/util/updater.sh")" # Appending error 
-fi
-
-# Checking Question or Updater source files are missing
-if [[ "$err_str" != "" ]]; then 
-    error_provider "$err_str"
-    # footer
-    # exit 0 # There is no point in continuing if either Question or Updater source file is missing
+if [[ $# -eq 0 ]]; then 
+    fetch_files 
 else 
-    echo "Select files for creating backup!"
-
-    # Bash 
-    bash_target=~/.bashrc # Actual file target
-    bash_source_path=$lib_target/shell/bash_sync.sh # Source targets 
-
-    if [[ -f $bash_target ]]; then # No need to ask for a backup, if the user don't have .bashrc file
-        if [[ -f $bash_source_path ]]; then
-            source $bash_source_path
-            bash_sync 
-        else err_str+="$(source_err "bash_sync.sh" "mew/lib/shell/bash_sync.sh")\n" # Appending error 
+    if [[ $1 = "push" ]]; then 
+        if [[ $# -eq 1 ]]; then
+            fetch_files "forward"
         fi
-    fi
-
-    # Fish
-    fish_target=~/.config/fish/config.fish # Actual file target
-    fish_source_path=$lib_target/shell/fish_sync.sh # Source targets 
-
-    if [[ -f $fish_target ]]; then  # No need to ask for a backup, if the user don't have config.fish file
-        if [[ -f $fish_source_path ]]; then
-            source $fish_source_path
-            fish_sync 
-        else err_str+="$(source_err "fish_sync.sh" "mew/lib/shell/fish_sync.sh")\n" # Appending error 
-        fi
-    fi
-
-    # Zsh
-    zsh_target=~/.zshrc # Actual file target
-    zsh_source_path=$lib_target/shell/zsh_sync.sh # Source targets 
-
-    if [[ -f $zsh_target ]]; then  # No need to ask for a backup, if the user don't have .zshrc file
-        if [[ -f $zsh_source_path ]]; then
-            source $zsh_source_path
-            zsh_sync 
-        else err_str+="$(source_err "zsh_sync.sh" "mew/lib/shell/zsh_sync.sh")" # Appending error 
-        fi
-    fi
-
-    # Vim 
-    vim_target=~/.vimrc # Actual file target
-    vim_source_path=$lib_target/vim/vim_sync.sh # Source targets 
-
-    if [[ -f $vim_target ]]; then  # No need to ask for a backup, if the user don't have .vimrc file
-        if [[ -f $vim_source_path ]]; then
-            source $vim_source_path
-            vim_sync 
-        else err_str+="$(source_err "vim_sync.sh" "mew/lib/vim/vim_sync.sh")" # Appending error 
-        fi
-    fi
-
-
-    # Vscode user settings
-    vscode_user_settings_target=~/.config/Code/User/settings.json # Actual file target
-    vscode_user_settings_source_path=$lib_target/vscode/user_settings_sync.sh # Source targets 
-
-    if [[ -f $vscode_user_settings_target ]]; then  # No need to ask for a backup, if the user don't have settings.json file
-        if [[ -f $vscode_user_settings_source_path ]]; then
-            source $vscode_user_settings_source_path
-            vscode_user_settings_sync 
-        else err_str+="$(source_err "user_settings_sync.sh" "mew/lib/vscode/user_settings_sync.sh")" # Appending error 
-        fi
-    fi
-
-    # dnf 
-    dnf_source_path=$lib_target/package-manager/dnf_sync.sh # Source targets 
-
-    if command -v dnf > /dev/null 2>&1; then  # No need to ask for a backup, if the user don't have dnf package manager (a non RHEL/Fedora user)
-        if [[ -f $dnf_source_path ]]; then
-            source $dnf_source_path
-            dnf_sync 
-        else err_str+="$(source_err "dnf_sync.sh" "mew/lib/dnf/dnf_sync.sh")" # Appending error 
-        fi
-    fi
-
-    # Lf 
-    lf_target=~/.config/lf/lfrc # Actual file target
-    lf_source_path=$lib_target/lf/lf_sync.sh # Source targets 
-
-    if [[ -f $lf_target ]]; then  # No need to ask for a backup, if the user don't have lfrc file
-        if [[ -f $lf_source_path ]]; then
-            source $lf_source_path
-            lf_sync 
-        else err_str+="$(source_err "lf_sync.sh" "mew/lib/lf/lf_sync.sh")" # Appending error 
-        fi
-    fi
-
-    # CHECKING and DISPLAYING any Errors if found 
-    if [[ "$err_str" != "" ]]; then 
-        error_provider "$err_str"
     fi
 fi
 
